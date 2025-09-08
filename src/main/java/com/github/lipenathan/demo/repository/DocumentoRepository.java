bda844d9bafafd77c91617f714815001415c133f
lipenathan <felipenatthan@gmail.com>
2025-09-08 20:35:22 -0300

    spring jpa 05/09

--- src/main/java/com/github/lipenathan/demo/repository/DocumentoRepository.java
+++ src/main/java/com/github/lipenathan/demo/repository/DocumentoRepository.java
@@ -1,11 +1,9 @@
 package com.github.lipenathan.demo.repository;
 
+import com.github.lipenathan.demo.repository.entity.DocumetoIdentificacao;
 import com.github.lipenathan.demo.repository.entity.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+public interface DocumentoRepository extends CrudRepository<DocumetoIdentificacao, Long> {
 
-    Usuario findByDataNascimento(String dataNascimento);
-
-    Usuario findByNome(String nome);
 }
